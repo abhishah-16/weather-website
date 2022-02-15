8c5fe679c064b665bc82e1026f494f1bb775fb35
Abhi Shah <abhishah0196@gmail.com>
2022-02-15 17:15:42 +0530

    modify about file

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.get('/weather', (req, res) => {
     geocode(req.query.address, (error, data) => {
         if (error) {
             return res.send({
-                error:error
+                error: error
             })
         }
 
